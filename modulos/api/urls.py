d19668ea2d46dedcf34111906271b5c8ed8f4758
hilario-wh <hilario@wisphub.net>
2022-05-07 12:47:49 -0500

    Agregado CRUD APIView

--- modulos/api/urls.py
+++ modulos/api/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 
 from modulos.api.views.mascota_decorator import mascota_list, mascota_detail
+from modulos.api.views.mascota_apiview import MascotaDetails, MascotaList
 
 urlpatterns = [
-
+    # Decorator
     url(r'^v1/mascotas$', mascota_list, name='mascota_list_v1'),
     url(r'^v1/mascotas/(?P<pk>\d+)$', mascota_detail, name='mascota_list_v1'),
+    # APIView
+    url(r'^v2/mascotas$', MascotaList.as_view(), name='mascota_list_v2'),
+    url(r'^v2/mascotas/(?P<pk>\d+)$', MascotaDetails.as_view(), name='mascota_list_v2'),
 ]
--- modulos/api/views/mascota_apiview.py
+++ modulos/api/views/mascota_apiview.py
@@ -1,55 +1,57 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from ast import Return
 from functools import partial
-from rest_framework import status
-from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from django.shortcuts import render
-from modulos.api.serializers import MascotaSerializer
+from rest_framework import status
+from yaml import serialize
 
 from modulos.mascota.models import Mascota
+from modulos.api.serializers import MascotaSerializer
 
 
-@api_view(['GET', 'POST'])
-def mascota_list(request):
+class MascotaList(APIView):
     """
-    DECORATOR | List all pets, or create a new one.
+    APIView | List all pets or create a new one
     """
-    if request.method == 'GET':
+    def get(self, request):
         mascotas = Mascota.objects.all()
         serializer = MascotaSerializer(mascotas, many=True)
         return Response(serializer.data)
-        
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = MascotaSerializer(data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def mascota_detail(request, pk):
+class MascotaDetails(APIView):
     """
-    DECORATOR | Retrieve, update or delete a pet
+    APIView | Retrieve, update or delete a pet
     """
-    try:
-        mascota = Mascota.objects.get(pk=pk)
-    except Mascota.DoesNotExist:
-        return Response(status=404)
+    def get_object(self, pk):
+        try:
+            mascota = Mascota.objects.get(pk=pk)
+            return mascota
+        except Mascota.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk):
+        mascota = self.get_object(pk)
         serializer = MascotaSerializer(mascota)
         return Response(serializer.data)
 
-    elif request.method == 'PUT' or request.method == 'PATCH':
+    def put(self, request, pk):
+        mascota = self.get_object(pk)
         serializer = MascotaSerializer(mascota, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk):
+        mascota = self.get_object(pk)
         mascota.delete()
         return Response(status=204)
