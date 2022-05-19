5a605eeee9ef40d863605dee5391b25bc742b8a3
hilario-wh <hilario@wisphub.net>
2022-05-19 17:32:08 -0500

    Agregada aplicacion almacen

--- refugio/settings.py
+++ refugio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'modulos.almacen',
     'modulos.adopcion',
     'modulos.mascota',
     'modulos.api',
